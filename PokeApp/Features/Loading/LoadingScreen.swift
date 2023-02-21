36c7f38bfe84aaf71d09f21c86480a91060c3f59
ayanbaktygaliyev <51299885+ayanbaktygaliyev@users.noreply.github.com>
2023-02-21 17:17:29 +0600

    Finished loading page with image

--- PokeApp/Features/Loading/LoadingScreen.swift
+++ PokeApp/Features/Loading/LoadingScreen.swift
@@ -2,6 +2,28 @@ import SwiftUI
 
 public struct LoadingScreen: View {
     public var body: some View {
-        Text("LoadingScreen")
+        VStack{
+            
+            Spacer(minLength: 150)
+                .fixedSize()
+            
+            TextLabel(
+                content: StringConstants.App.foodie,
+                color: .foodieGreen,
+                fontToken: .size70,
+                style: .bold
+            )
+            
+            Spacer(minLength: 58)
+                .fixedSize()
+            
+            GifImage("loading")
+                .allowsHitTesting(false)
+        }
+    }
+}
+struct LoadingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInScreen()
     }
 }
