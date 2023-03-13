83b77e307515779ba7ad38e2ca56fcd1c1991b9c
markdaribayev <markdaribayev@gmail.com>
2023-03-13 16:11:53 +0600

    Add networking layer

--- PokeApp/Features/SignIn/SignInViewModel.swift
+++ PokeApp/Features/SignIn/SignInViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftUI
+import Combine
 
 class SignInViewModel: ObservableObject {
     struct State {
@@ -9,6 +10,10 @@ class SignInViewModel: ObservableObject {
     
     @Published
     var state: State
+    
+    private var cancellables = [AnyCancellable]()
+    
+    let authRepository = AuthRepository()
 
     init() {
         self.state = State(username: "", password: "")
@@ -16,5 +21,30 @@ class SignInViewModel: ObservableObject {
     
     func didTapSignIn() {
         print("Hello Sign In")
+        let param = PostParam(title: "Hi", body: "ily", userId: 1)
+        
+//        authRepository.testPostNewPost(param: param)
+//            .receive(on: DispatchQueue.main)
+//            .sink { result in
+//                switch result {
+//                case .success(let post):
+//                    print("// post: \(post)")
+//                case .failure(let error):
+//                    print("// error: \(error)")
+//                }
+//            }
+//            .store(in: &cancellables)
+        
+        authRepository.testLoadTodo(id: 1)
+            .receive(on: DispatchQueue.main)
+            .sink { result in
+                switch result {
+                case .success(let todo):
+                    print("// todo: \(todo)")
+                case .failure(let error):
+                    print("// error: \(error)")
+                }
+            }
+            .store(in: &cancellables)
     }
 }
